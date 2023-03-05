1363ff69447e2933881e79f571fa3a19d4ca73cc
Domanrovil Ovalle <domanrovil.ovalle@gmail.com>
2023-03-05 02:35:53 +0000

    Completed Lab Seerch 2d Array

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,13 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+        for (int i =0; i < nums.length; i++){
+            for (int ii = 0; ii < nums[i].length; ii++){
+                if(nums[i][ii] == target){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
